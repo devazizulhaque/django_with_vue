ad799dba3dba06618bf6a8475ead4e34de15656b
devazizulhaque <azizulhaque4198@gmail.com>
2025-06-01 18:02:41 +0600

    create new django with vue project

---
+++ frontend/frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import api from '../services/api'
+
+const message = ref('Loading...')
+
+onMounted(async () => {
+  const res = await api.get('/hello/')
+  message.value = res.data.message
+})
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
